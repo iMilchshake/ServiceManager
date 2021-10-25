c9254903bcd9e304470ec7f4594c920293b20be9
Tobias Schneider <tobias.schneider@smail.inf.h-brs.de>
2021-10-25 22:10:02 +0200

    adds 'Recent Activity' to the profile scraper

--- ServiceManager/steam_scraper/__main__.py
+++ ServiceManager/steam_scraper/__main__.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
     args = vars(parser.parse_args())
 
     # fetch user data
-    user_data = get_all_user_data(args["steam_id"], args["api_key"])
+    user_data = str(get_all_user_data(args["steam_id"], args["api_key"]))
 
     if args["dir"]:
         file_path = os.path.join(args["dir"], datetime.now().strftime(f"{args['steam_id']}_%d_%m_%Y_%H_%M"))
--- ServiceManager/steam_scraper/steam_scraper.py
+++ ServiceManager/steam_scraper/steam_scraper.py
@@ -3,6 +3,8 @@ import json
 import requests
 from bs4 import BeautifulSoup
 
+PLAYTIME_EPSILON = 0.1
+
 
 def scrape_profile(steamid):
     URL_PROFILE = f"https://steamcommunity.com/profiles/{steamid}/"
@@ -22,6 +24,7 @@ def scrape_profile(steamid):
             profile_stats[heading] = 0
 
     profile_stats["player_level"] = int(profile_soup.find(class_="friendPlayerLevelNum").text)
+    profile_stats["recent_activity"] = profile_soup.find(class_="recentgame_recentplaytime").text.split(" ")[0]
     return profile_stats
 
 
@@ -37,7 +40,7 @@ def get_player_summaries_v2(steam_id, api_key):
 def get_owned_games_v1(steam_id, api_key):
     res = requests.get(
         f"http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={api_key}&steamid={steam_id}"
-        f"&format=json&include_played_free_games=true")
+        f"&format=json&include_played_free_games=true&skip_unvetted_apps=false")
     if res.status_code == 200:
         return json.loads(res.content.decode('utf-8'))["response"]
     else:
@@ -50,4 +53,13 @@ def get_all_user_data(steam_id, api_key):
         "get_player_summaries_v2": get_player_summaries_v2(steam_id, api_key),
         "get_owned_games_v1": get_owned_games_v1(steam_id, api_key)
     }
-    return str(data)
+    return data
+
+
+def validate_data(data):
+    # compare recent playtime from scraped profile and from API
+    playtime_owned_games = sum(map(lambda game: game['playtime_2weeks'] if 'playtime_2weeks' in game else 0,
+                                   data['get_owned_games_v1']['games'])) / 60
+    playtime_profile_scrape = float(data['profile_scrape']['recent_activity'])
+    assert abs(playtime_profile_scrape - playtime_owned_games) < PLAYTIME_EPSILON, \
+        f"scraped and API's recent playtime differ more than {PLAYTIME_EPSILON}"
